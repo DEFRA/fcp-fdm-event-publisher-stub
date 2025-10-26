4eed1f18c15d8e9b9a9d0883b540e7f345ba7848
John Watson <42574977+johnwatson484@users.noreply.github.com>
2025-10-26 19:15:40 +0000

    Support performance tests (#4)

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -1,5 +1,8 @@
+import { constants as httpConstants } from 'node:http2'
 import Joi from 'joi'
 import { simulateMessages } from '../simulate/messages.js'
+
+const { HTTP_STATUS_ACCEPTED } = httpConstants
 
 const messages = {
   method: 'POST',
@@ -11,16 +14,22 @@ const messages = {
     validate: {
       query: {
         scenario: Joi.string().allow('').description('The scenario CRN to simulate messages for. If not provided, all known scenarios will be used'),
-        repetitions: Joi.number().integer().min(1).max(100).default(1).description('The number of times to repeat the scenario')
+        repetitions: Joi.number().integer().min(1).max(1000).default(1).description('The number of times to repeat the scenario')
       }
     }
   },
   handler: async (request, h) => {
     const { scenario, repetitions } = request.query
 
-    const summary = await simulateMessages({ scenario, repetitions })
+    simulateMessages({ scenario, repetitions })
+      .then(summary => {
+        request.logger.info(`Simulated messages summary: ${JSON.stringify(summary)}`)
+      })
+      .catch(err => {
+        request.logger.error(`Simulate messages failed: ${err.message}`)
+      })
 
-    return h.response({ data: { summary } })
+    return h.response({ status: 'ok', message: 'Simulation started' }).code(HTTP_STATUS_ACCEPTED)
   }
 }
 
